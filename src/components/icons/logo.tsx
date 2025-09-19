3546a40b8f7807e11f750e2c1ab6f01fa7219ed1
emploi-id <emploi.id89@gmail.com>
2025-09-19 06:15:43 +0000

    edit text into lowercase emploi (_for element span_)

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="flex items-center gap-2">
       <Briefcase className="h-6 w-6 text-primary" />
-      <span className="font-headline text-xl font-bold">EmployMatch</span>
+      <span className="font-headline text-xl font-bold">emploi</span>
     </div>
   );
 }
