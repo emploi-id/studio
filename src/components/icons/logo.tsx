a2f2291b79ab052f532574ce7554fa80de4b30ce
emploi-id <emploi.id89@gmail.com>
2025-09-19 06:24:41 +0000

    change logo only text (_for element <Logo>_)

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,9 +1,7 @@
-import { Briefcase } from 'lucide-react';
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      <Briefcase className="h-6 w-6" />
+    <div className="flex items-center">
       <span className="font-headline text-xl font-bold">emploi</span>
     </div>
   );
