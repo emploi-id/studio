bc3f9539315d5ab2604f932c335d7aabf48e1c5f
emploi-id <emploi.id89@gmail.com>
2025-09-23 04:43:28 +0000

    please make horizontal (_for element <Primitive.div>_)

--- src/components/forms/employer-signup-form.tsx
+++ src/components/forms/employer-signup-form.tsx
@@ -81,7 +81,7 @@ export default function EmployerSignUpForm({ onBack }: Props) {
                     <RadioGroup
                       onValueChange={field.onChange}
                       defaultValue={field.value}
-                      className="flex flex-col space-y-1"
+                      className="flex space-x-4"
                     >
                       <FormItem className="flex items-center space-x-3 space-y-0">
                         <FormControl>
