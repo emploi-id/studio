2fd429a069b7a02c5b05fbaf1fdf832c899e0266
emploi-id <emploi.id89@gmail.com>
2025-10-08 08:26:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8194808650-2a1a8",
+  "appId": "1:988885356249:web:1a4b1cd3064cd564a378c8",
+  "apiKey": "AIzaSyAwskFrP1djO8QCrtVYbR8taez0u0uMvb0",
+  "authDomain": "studio-8194808650-2a1a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "988885356249"
+};
